6c4b75a928a8149933fbc815329ddfbdc17b8540
beimnet melese <beimnetmelese16@gmail.com>
2025-06-05 17:53:24 +0300

    Task App Completed

--- Task/views.py
+++ Task/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.completed = True
+        instance.save()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
